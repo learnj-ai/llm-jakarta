247b7e98219369cbbb91d3ae2c46e48d11088422
Bazlur Rahman <bazlur@jugbd.org>
2025-04-06 23:41:33 -0400

    Enhance Book entity with timestamps and default image handling; refactor initialization logic to use builder pattern

--- step-08-advanced-tools-and-rag/src/main/java/learning/jakarta/ai/bookstore/domain/Book.java
+++ step-08-advanced-tools-and-rag/src/main/java/learning/jakarta/ai/bookstore/domain/Book.java
@@ -1,18 +1,21 @@
 package learning.jakarta.ai.bookstore.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Random;
 
 @Entity
 @Table(name = "books")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Book {
     @Id
     private String isbn;
@@ -36,4 +39,34 @@ public class Book {
     private String category;
 
     private String imageUrl;
+
+    private Instant createdDate;
+    private Instant lastUpdatedDate;
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = Instant.now();
+        lastUpdatedDate = Instant.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        lastUpdatedDate = Instant.now();
+    }
+
+    static String[] defaultImages = {
+            "/images/default.png",
+            "/images/default1.png",
+            "/images/default2.png",
+            "/images/default3.png"
+    };
+    static Random random = new Random();
+
+    public String getImageUrl() {
+        if (imageUrl == null || imageUrl.isEmpty()) {
+            int randomIndex = random.nextInt(defaultImages.length);
+            return defaultImages[randomIndex];
+        }
+        return imageUrl;
+    }
 }
